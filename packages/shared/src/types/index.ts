a76fa95d8dae531a2f5c5f54813a9c0d6cae106a
Jimmy Chavada <rchavada@uwo.ca>
2026-04-10 09:08:20 -0400

    Push

--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -1,1 +1,1 @@
-export type { Trip, Destination, DayItinerary, ItineraryItem, FoodSpot, Activity } from "./trip.js";
+export type { Trip, Destination, DayItinerary, ItineraryItem, ItemCategory, FoodSpot, FoodType, Activity } from "./trip.js";
